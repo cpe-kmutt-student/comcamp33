e681055150c6efdf2193a0a9dd4cf49627bbc9a3
WTFPUn <punnawat.nam@innosoft.kmutt.ac.th>
2022-03-02 23:48:25 +0700

    add: load and save logicl, POSTcheck page

--- src/pages/api/data.js
+++ src/pages/api/data.js
@@ -3,7 +3,6 @@ import { getSession } from 'next-auth/react';
 
 export default async function handler(req, res) {
   const session = await getSession({ req });
-
   // Validate client authentication session before access to restricted endpoint and database connectionjs object of function to call
   if (!session) {
     return res.status(401).json({
@@ -12,9 +11,6 @@ export default async function handler(req, res) {
       timestamp: new Date(),
     });
   }
-
-  // Connect to the database before query
-  const database = await mongoClientPromise;
 
   if (req.method === 'GET') return loadData(req, res);
   if (req.method === 'POST') return saveData(req, res);
@@ -26,18 +22,36 @@ export default async function handler(req, res) {
   });
 };
 
-const loadData = (req, res) => {
+const loadData = async (req, res) => {
+  const database = await mongoClientPromise;
+  //find return cursor object need Array to call it
+  const dataList = await database.db("comcamp33").collection("data").find().toArray();
   return res.status(200).json({
     success: true,
-    message: 'load data',
+    message: dataList,
     timestamp: new Date(),
   });
 };
 
-const saveData = (req, res) => {
-  return res.status(200).json({
-    success: true,
-    message: 'save data',
-    timestamp: new Date(),
-  });
+const saveData = async (req, res) => {
+  const database = await mongoClientPromise;
+  const data = req.body;
+  const faceEmail = data.facebook.email;
+  const findUser = await database.db("comcamp33").collection("data").findOne({"facebook.email": faceEmail}) ;
+  if(!findUser){
+    const save = await database.db("comcamp33").collection("data").insertOne(data)
+    return res.status(200).json({
+      success: true,
+      message: save,
+      timestamp: new Date(),
+    });
+  }
+  else {
+    const update = await database.db("comcamp33").collection("testQuery").update({ "facebook.email": faceEmail}, {$set: data})
+    return res.status(200).json({
+      success: true,
+      message: update,
+      timestamp: new Date(),
+    });
+  }
 };
