b199a94d3965d2aae72cdc571976fd0534da9b53
riflowth <riflowth@gmail.com>
2022-03-06 17:02:17 +0700

    feat: add autosave with debouncing mechanism

---
+++ src/utils/clientUtils.js
@@ -0,0 +1,29 @@
+export const debounce = (func, timeout) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => { func.apply(this, args); }, timeout);
+  };
+};
+
+export const saveData = async (data) => {
+  const response = await fetch('/api/data', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({ data }),
+  });
+  return response.json();
+};
+
+export const loadData = async (data) => {
+  const response = await fetch('/api/data', {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json',
+    },
+  });
+  return response.json();
+};
+
