a932babcf66899eb7f54a84b7cfcc7c8942a5ebd
Keetawin <khitawanlimxrun@gmail.com>
2022-03-07 00:00:47 +0700

    condition but have rsponsive ploblem

--- src/components/condition.js
+++ src/components/condition.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Condition = () => {
   return (
-    <div className="container mx-auto">
+    <div className="mx-10 lg:mx-4">
          <section className="text-white py-4 sm:pt-24 md:pt-36 lg:py-10 flex justify-center text-center font-pixel text-2xl sm:text-3xl md:text-6xl lg:text-8xl">
             Condition
             
@@ -14,7 +14,7 @@ const Condition = () => {
               quality={100}
               
               />
-              <div className="text-base md:text-2xl py-4  lg:py-0 my-6 px-10 bg-[#04B1AF]">
+              <div className="text-base font-thai text-white md:text-2xl py-4  lg:py-4 my-6 px-10 bg-[#04B1AF]">
               นักเรียนมัธยมศึกษาปีที่ 4, 5 สายวิทย์
 หรือคณิตคอม ปวช. และนักศึกษา รหัส 65
 ผู้ที่ยืนยันสิทธ์กับทาง ภาควิศวกรรมคอมพิวเตอร์ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี
@@ -28,19 +28,19 @@ const Condition = () => {
               quality={100}
               
               />
-               <div className="text-base py-2 md:text-2xl md:py-16 my-6 px-10 bg-[#556CC9]">
+               <div className="text-base font-thai text-white py-2 md:text-2xl md:py-16 my-6 px-10 bg-[#556CC9]">
                มีความสนใจในด้านคอมพิวเตอร์
 โดยไม่จำเป็นต้องมีพื้นฐานมาก่อน
 
               </div>
             </div>
-            <div className="flex flex-col justify-center"><Image src="/assets/condition/piccom.svg"
+            <div className="flex flex-col text-white justify-center"><Image src="/assets/condition/piccom.svg"
               width={460}
               height={540}
               quality={100}
               
               />
-              <div className="text-base py-4 md:text-2xl md:py-16 my-6 px-10 bg-[#DD517E]">
+              <div className="text-base font-thai py-4 md:text-2xl md:py-16 my-6 px-10 bg-[#DD517E]">
               ผู้ปกครองอนุญาตให้นักเรียนคนดังกล่าวเข้าร่วมกิจกรรม ตามระยะเวลาที่กำหนด
 
               </div>
--- src/components/contact.js
+++ src/components/contact.js
@@ -26,10 +26,10 @@ const Contact = () => {
           </div>
           </div>
         </div>
-        <div className="flex justify-center hidden se:block">
+        <div className="flex justify-center scale-75">
         <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FKMUTTcomcamp&tabs=timeline&width=320&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=323404429809727" width={340} height={500} style={{border: 'none', overflow: 'hidden'}} scrolling="no" frameBorder={0} allowFullScreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" />
         </div>
-        <div className="flex justify-center "> 
+        <div className="flex justify-center scale-75"> 
           <iframe width={320} height={440} src="https://www.instagram.com/p/CaRvnhkhs2v/embed" frameBorder={0} /></div>
       </div>
     </div>
