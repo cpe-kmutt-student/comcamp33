cc0a25d347b89dba6512de0856338ff1807c2a5b
quadiez <dduidokdoi@gmail.com>
2022-03-09 22:17:48 +0700

    fix: logo position

--- src/components/homepage/first/index.js
+++ src/components/homepage/first/index.js
@@ -14,9 +14,8 @@ const First = () => {
 
     return (
         <>
-            <div className="absolute h-screen w-screen top-5 right-0">
-                {/* <Image src="/assets/first/radiant.svg" quality={100} layout="fill" objectFit="cover" alt="light" /> */}
-                <svg viewBox="0 0 1855 1080" xmlns="http://www.w3.org/2000/svg" objectFit="cover" layout="fill">
+            <div className="absolute h-[92vh] w-screen right-0">
+                <svg viewBox="0 0 1855 1080" xmlns="http://www.w3.org/2000/svg" style={{ position: "absolute", height: '100%', objectPosition: 'center', objectFit: 'cover', width: '100%', top: 0, right: 0, paddingBottom: '20vh' }}>
                     <path className='animate-expand5' opacity="0.1" d="M927.41 1471C1439.6 1471 1854.82 1053.95 1854.82 539.5C1854.82 25.0468 1439.6 -392 927.41 -392C415.216 -392 0 25.0468 0 539.5C0 1053.95 415.216 1471 927.41 1471Z" fill="#DD517E" />
                     <path className='animate-expand4' opacity="0.1" d="M907.61 1150C1272.99 1150 1569.19 852.493 1569.19 485.5C1569.19 118.507 1272.99 -179 907.61 -179C542.23 -179 246.03 118.507 246.03 485.5C246.03 852.493 542.23 1150 907.61 1150Z" fill="#DD517E" />
                     <path className='animate-expand3' opacity="0.1" d="M894.71 927.39C1154.07 927.39 1364.32 716.207 1364.32 455.7C1364.32 195.193 1154.07 -15.99 894.71 -15.99C635.352 -15.99 425.1 195.193 425.1 455.7C425.1 716.207 635.352 927.39 894.71 927.39Z" fill="#DD517E" />
@@ -25,7 +24,9 @@ const First = () => {
                 </svg>
             </div>
             <div className="h-[60vh] relative mx-5">
-                <Image src="/assets/sun.svg" quality={100} layout="fill" objectFit='contain' alt="sun" />
+                <div className="h-[58vh] relative ml-6 ">
+                    <Image src="/assets/sun.svg" quality={100} layout="fill" objectFit='contain' alt="sun" />
+                </div>
                 <Image className='animate-wiggle' src="/assets/comcamp.png" quality={100} layout="fill" objectFit='contain' alt="comcamp logo" />
             </div>
             <div className="absolute bottom-0 right-0 w-[100%] md:h-[35vh] h-[40vh]" style={{ transform: `translateY(${offsetY * 0.1}px)` }}>
