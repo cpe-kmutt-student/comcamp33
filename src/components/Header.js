067ed8adff90b9c764ed35cf710bce1cb3bcc7da
Keetawin <khitawanlimxrun@gmail.com>
2022-03-10 23:26:28 +0700

    add:opengraph facebook image

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,13 +11,13 @@ const Header = () => {
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Comcamp 33" />
         <meta property="og:description" content="โครงการฝึกอบรมเชิงปฏิบัติการคอมพิวเตอร์เบื้องต้น หรือ Comcamp ครั้งที่ 33" />
-        <meta property="og:image" content="https://comcamp.io/assets/banner.png" />
+        <meta property="og:image" content="https://comcamp.io/assets/opengraph.png" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="comcamp.io" />
         <meta property="twitter:url" content="https://comcamp.io/" />
         <meta name="twitter:title" content="Comcamp 33" />
         <meta name="twitter:description" content="โครงการฝึกอบรมเชิงปฏิบัติการคอมพิวเตอร์เบื้องต้น หรือ Comcamp ครั้งที่ 33" />
-        <meta name="twitter:image" content="https://comcamp.io/assets/banner.png" />
+        <meta name="twitter:image" content="https://comcamp.io/assets/opengraph.png" />
     </Head>
   );
 };
