0b931f5d5a78352b9e890521adab4e0187d7135f
Keetawin <khitawanlimxrun@gmail.com>
2022-03-11 11:33:53 +0700

    add:font display swap

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['platform-lookaside.fbsbx.com']
-  }
+  },
+  i18n: {
+    locales: ["th"],
+    defaultLocale: "th",
+  },
 }
 
 module.exports = nextConfig
