889296deeb9989283f06a5b2b6dafd311a8f5c8c
quadiez <dduidokdoi@gmail.com>
2022-03-11 14:33:23 +0700

    fix: remove wave about

--- src/components/homepage/about/BGAnimation.js
+++ src/components/homepage/about/BGAnimation.js
@@ -6,13 +6,13 @@ import { PurpleBlob } from "./Blobs/PurpleBlob"
 export const BGAnimation = () => {
     return (
         <>
-            <div className="absolute top-0 right-0 w-full h-full z-0">
+            <div className="absolute top-0 right-0 w-full h-full z-0 overflow-hidden">
                 <BlobAnimation inity={0} speed={10}><PinkBlob /></BlobAnimation>
             </div>
-            <div className="absolute top-0 right-0 w-full h-full z-0">
+            <div className="absolute top-0 right-0 w-full h-full z-0 overflow-hidden">
                 <BlobAnimation initx={0} inity={0} speed={12}><BlueBlob /></BlobAnimation>
             </div>
-            <div className="absolute top-0 right-0 w-full h-full z-0">
+            <div className="absolute top-0 right-0 w-full h-full z-0 overflow-hidden">
                 <BlobAnimation speed={8}><PurpleBlob /></BlobAnimation>
             </div>
         </>
--- src/pages/home.js
+++ src/pages/home.js
@@ -10,7 +10,7 @@ const Home = () => {
     Aos.init({ duration: 1000 });
   }, []);
   return (
-    <div className="bg-bg-primary max-w-full overflow-x-clip overflow-x-hidden overflow-visible">
+    <div className="bg-bg-primary max-w-full overflow-x-hidden overflow-visible">
       <Header />
       <Navigation />
 
@@ -23,11 +23,7 @@ const Home = () => {
       <section className="bg-[#11033E] h-[110vh]" id="about">
         <About />
       </section>
-      <div className="absolute  w-[100%] overflow-hidden leading-[0]">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill" fill="#11033E"></path>
-        </svg>
-      </div>
+
 
 
       <section className="bg-[#200440] h-[120vh]" id="learning">
