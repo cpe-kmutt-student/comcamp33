2556a6d1a358253cb0f97bd19c2494c0af6962af
Keetawin <khitawanlimxrun@gmail.com>
2022-03-12 16:50:26 +0700

    fix:comcamp33 word

--- src/components/homepage/Footer.js
+++ src/components/homepage/Footer.js
@@ -3,8 +3,8 @@ import { FaCopyright } from 'react-icons/fa'
 const Footer = () => {
     return (
         <div className="w-screen lg:h-[8vh] bg-black flex justify-center items-center lg:flex-row font-pixel text-white text-[3.6vw] sm:text-lg space-x-2 px-5 flex-col h-[10vh] space-y-[-1vw] lg:space-y-0">
-            <div className="flex justify-center items-center flex-row space-x-2"><FaCopyright /><p>2022 comcamp#33 all rights reserved.</p></div>
-            <p>designed and developed by cpe#35</p>
+            <div className="flex justify-center items-center flex-row space-x-2"><FaCopyright /><p>2022 Comcamp 33 all rights reserved.</p></div>
+            <p>Designed and Developed by CPE35</p>
         </div>
     )
 }
