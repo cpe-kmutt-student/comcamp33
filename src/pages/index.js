4b077a91e5dc7c33e9f2267a295492d9c608e233
quadiez <dduidokdoi@gmail.com>
2022-03-12 13:35:50 +0700

    fix: home

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,28 +6,28 @@ const IndexPage = () => {
   return (
     <div>
       <Header />
-      
-      
-      <header class="relative flex items-center justify-center h-screen overflow-hidden">
-        <Link href="/home">
-  <div
-    className="relative z-30 py-2 lg:py-6 text-center top-[18vh] lg:top-[19.5vh] w-[96vw] md:w-[30vw] bg-transparent  text-[#E9E9E9] font-pixel  md:text-xl hover:text-2xl cursor-pointer text-shadow transition-all duration-200 ease-linear">
-    Enter Website
-  </div>
-  </Link>
-  <Link href="/home">
-  <video
-    preload="true" autoPlay loop muted
-    className="absolute z-10 min-w-full cursor-pointer bg-dunes bg-no-repeat bg-cover bg-center object-cover min-h-full "
-  >
-    <source
-      src="/assets/Clickweb.mp4"
-      type="video/mp4"
-    />
-    
-  </video>
-  </Link>
-</header>
+
+
+      <header className="relative flex items-center justify-center h-screen overflow-hidden">
+        <Link href="/home" passHref>
+          <div
+            className="relative z-30 py-2 lg:py-6 text-center top-[18vh] lg:top-[19.5vh] w-[96vw] md:w-[30vw] bg-transparent  text-[#E9E9E9] font-pixel  md:text-xl hover:text-2xl cursor-pointer text-shadow transition-all duration-200 ease-linear">
+            Enter Website
+          </div>
+        </Link>
+        <Link href="/home" passHref>
+          <video
+            preload="true" autoPlay loop muted
+            className="absolute z-10 min-w-full cursor-pointer bg-dunes bg-no-repeat bg-cover bg-center object-cover min-h-full "
+          >
+            <source
+              src="/assets/Clickweb.mp4"
+              type="video/mp4"
+            />
+
+          </video>
+        </Link>
+      </header>
     </div>
   );
 }
