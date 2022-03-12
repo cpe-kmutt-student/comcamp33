d75e1504d2fe836d67cfdce0dd5e7372aeda643b
quadiez <dduidokdoi@gmail.com>
2022-03-12 21:55:11 +0700

    fix: padding

--- src/components/homepage/first/index.js
+++ src/components/homepage/first/index.js
@@ -25,7 +25,7 @@ const First = () => {
     return (
         <>
             <ModalFacebook isVisible={isModalVisible} setIsModalVisible={setIsModalVisible} handleSuccess={signIn} />
-            <div className="absolute h-screen w-screen lg:top-[-5vh] md:top-[-7vh]  sm:top-[-10vh] hidden lg:block md:block sm:block">
+            <div className="absolute h-screen w-screen lg:top-[-5vh] md:top-[-7vh]  sm:top-[-10vh] hidden lg:block md:block sm:block right-0">
                 <svg
                     viewBox="0 0 1855 1080"
                     xmlns="http://www.w3.org/2000/svg"
