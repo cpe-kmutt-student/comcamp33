6380eecfa3b3270e5d3ce895cb2662c7b8b930c3
quadiez <dduidokdoi@gmail.com>
2022-03-13 21:06:57 +0700

    test: town

--- src/components/homepage/first/index.js
+++ src/components/homepage/first/index.js
@@ -27,7 +27,7 @@ const First = () => {
     return (
         <>
             <ModalFacebook isVisible={isModalVisible} setIsModalVisible={setIsModalVisible} handleSuccess={signIn} />
-            <div className="absolute h-screen w-screen lg:top-[-5vh] md:top-[-7vh]  sm:top-[-10vh] hidden lg:block md:block sm:block right-0">
+            {/*<div className="absolute h-screen w-screen lg:top-[-5vh] md:top-[-7vh]  sm:top-[-10vh] hidden lg:block md:block sm:block right-0">
                 <svg
                     viewBox="0 0 1855 1080"
                     xmlns="http://www.w3.org/2000/svg"
@@ -72,7 +72,7 @@ const First = () => {
                         fill="#DD517E"
                     />
                 </svg>
-            </div>
+                </div>*/}
             <div className="h-[55vh] relative ">
                 <div className="h-[55vh] relative ">
                     <Image
